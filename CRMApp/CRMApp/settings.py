dd543286542ac65977335e6ac117b0811df2a610
ameyshirsat1842 <ameyshirsat1842@gmail.com>
2024-08-14 16:48:41 +0530

    update

--- CRMApp/CRMApp/settings.py
+++ CRMApp/CRMApp/settings.py
@@ -151,8 +151,3 @@ CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 broker_connection_retry_on_startup = True
 
-
-# Access the environment variables
-TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
-TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
