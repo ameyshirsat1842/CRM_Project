b2c83e98ed8434ac463cc8f0af0ccea57d7f090d
amey1842 <ameyshirsat1842@gmail.com>
2024-09-08 23:06:21 +0530

    update

--- CRMApp/CRMApp/asgi.py
+++ CRMApp/CRMApp/asgi.py
@@ -3,7 +3,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import re_path
-from website.consumers import NotificationConsumer
+from website.consumers import NotificationConsumer  # Updated with the correct app name
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRMApp.settings')
 
@@ -11,7 +11,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter([
-            re_path(r'ws/notifications/$', NotificationConsumer.as_asgi()),  # Corrected WebSocket URL
+            re_path(r"ws/notifications/$", NotificationConsumer.as_asgi()),
         ])
     ),
 })
